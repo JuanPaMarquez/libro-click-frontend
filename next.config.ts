00f3f344e6dca3096ee93691626347d768c8a3a2
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-01-28 15:18:24 -0500

    commit init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  const nextConfig: NextConfig = {
+    /* config options here */
+    output: 'export',
+    reactStrictMode: true, // Habilita el modo estricto de React
+    images: {
+      unoptimized: true, // Hace que las imágenes sean compatibles con exportaciones estáticas
+    },
+  };
 
-export default nextConfig;
+  export default nextConfig;
