72bc90e2de1d956474840778c38caaea5c0522f8
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-02-09 14:33:36 -0500

    Agregar configuración de Tailwind CSS con contenido y safelist

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "underline",
+    "decoration-2",
+    "underline-offset-6",
+    "shadow-cyan-500/50",
+    "text-black",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
